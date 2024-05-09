d029cbd875c5285f21f489090fc546d16c4cba38
Pranjaljain23 <pranjaljain1112@gmail.com>
2024-05-09 12:34:57 +0530

    file renamed

--- Node_Tutorial/03-modules.js
+++ Node_Tutorial/03-modules.js
@@ -7,10 +7,10 @@
 // import { john, peter } from './4-names';
 // import sayHi from './5-utils';
 
-const names = require('./4-names');
-const sayHi = require('./5-utils');
-const data = require('./6-alternative-flavor');
-require('./7-mind-grenade')  //When you import a module you actually invoke it
+const names = require('./04-names');
+const sayHi = require('./05-utils');
+const data = require('./06-alternative-flavor');
+require('./07-mind-grenade')  //When you import a module you actually invoke it
 
 
 sayHi("Susan");
